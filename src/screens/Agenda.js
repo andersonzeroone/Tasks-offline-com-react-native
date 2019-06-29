5724d2a6a2d51bc4539a53417ce367cc87c64379
anderson <anderson.pablo02@gmail.com>
2019-06-28 23:33:52 -0300

    Add filtro de tarefas concluídas.

--- src/screens/Agenda.js
+++ src/screens/Agenda.js
@@ -1,52 +1,142 @@
-import React ,{ Component} from  'react'
-import { StyleSheet, Text, View, ImageBackground } from 'react-native'
+import React, { Component } from 'react'
+import {
+    StyleSheet,
+    Text,
+    View,
+    ImageBackground,
+    FlatList,
+    TouchableOpacity,
+    Platform
+} from 'react-native'
 import moment from 'moment'
 import 'moment/locale/pt-br'
 import todayImage from '../../assets/imgs/today.jpg'
-import commonStyle from    '../commonStyles'
+import commonStyles from '../commonStyles'
 import Task from '../component/Task'
-
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 export default class Agenda extends Component {
+    state = {
+        tasks: [
+            {
+                id: Math.random(), desc: 'Terminar modulo',
+                estimateAt: new Date(), doneAt: new Date()
+            },
+            {
+                id: Math.random(), desc: 'Concluir',
+                estimateAt: new Date(), doneAt: null
+            },
+            {
+                id: Math.random(), desc: 'Terminar modulo',
+                estimateAt: new Date(), doneAt: new Date()
+            },
+            {
+                id: Math.random(), desc: 'Concluir',
+                estimateAt: new Date(), doneAt: null
+            },
+            {
+                id: Math.random(), desc: 'Terminar modulo',
+                estimateAt: new Date(), doneAt: new Date()
+            },
+            {
+                id: Math.random(), desc: 'Concluir',
+                estimateAt: new Date(), doneAt: null
+            },
+            {
+                id: Math.random(), desc: 'Terminar modulo',
+                estimateAt: new Date(), doneAt: new Date()
+            },
+            {
+                id: Math.random(), desc: 'Concluir',
+                estimateAt: new Date(), doneAt: null
+            },
+            {
+                id: Math.random(), desc: 'Concluir',
+                estimateAt: new Date(), doneAt: null
+            },
+            {
+                id: Math.random(), desc: 'Terminar modulo',
+                estimateAt: new Date(), doneAt: new Date()
+            },
+            {
+                id: Math.random(), desc: 'Concluir',
+                estimateAt: new Date(), doneAt: null
+            },
+            {
+                id: Math.random(), desc: 'Terminar modulo',
+                estimateAt: new Date(), doneAt: new Date()
+            },
+            {
+                id: Math.random(), desc: 'Concluir',
+                estimateAt: new Date(), doneAt: null
+            },
+        ],
+        visibleTasks: [],
+        showDonetasks: true,
+    }
+    filterTasks= () => {
+        let visibleTasks= null
+        if ( this.state.showDonetasks) {
+            visibleTasks = [...this.state.tasks]
+        }else {
+            const pending = task => task.doneAt === null
+            visibleTasks = this.state.tasks.filter(pending)
+        }
+        this.setState({ visibleTasks })
+    }
+
+    toggleFilter = () => {
+        this.setState({ showDonetasks: !this.state.showDonetasks },
+             this.filterTasks)
+    }
+
+    componentDidMount = () => {
+        this.filterTasks()
+    }
+
+    toggleTask = id => {
+        const  tasks = this.state.tasks.map(task => {
+            if(task.id === id ){
+                task = { ...task }
+                task.doneAt = task.doneAt ? null: new Date() 
+            }
+            return task
+        })
+
+        this.setState({ tasks }, this.filterTasks)
+    }
+ 
     render() {
-        return(
-            <View style={style.container}>
-                <ImageBackground source={todayImage} style={style.background}>
-                    <View style={style.titleBar}>
-                        <Text style={style.title}>Hoje</Text>
-                        <Text style={style.subtitle}>
+        return (
+            <View style={styles.container}>
+                <ImageBackground source={todayImage} 
+                    style={styles.background}>
+                    <View style={styles.iconBar}>
+                        <TouchableOpacity onPress= { this.toggleFilter }>
+                            <Icon name={this.state.showDonetasks ? 'eye' : 'eye-slash'}
+                                size={20} color={commonStyles.colors.secondary} >
+                            </Icon>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.titleBar}>
+                        <Text style={styles.title}>Hoje</Text>
+                        <Text style={styles.subtitle}>
                             {moment().locale('pt-br').format('ddd,D [de] MMMM')}
                         </Text>
                     </View>
                 </ImageBackground>
-                <View style={style.taksContainer}>
-                    <Task desc='Tarefas pendentes'
-                        estimateAt={new Date()} doneAt={null} />
-                    <Task desc='Tarefa concluida'
-                        estimateAt={new Date()} doneAt={new Date()} />
-                    <Task desc='Tarefas pendentes'
-                        estimateAt={new Date()} doneAt={null} />
-                    <Task desc='Tarefa concluida'
-                        estimateAt={new Date()} doneAt={new Date()} />
-                    <Task desc='Tarefas pendentes'
-                        estimateAt={new Date()} doneAt={null} />
-                    <Task desc='Tarefa concluida'
-                        estimateAt={new Date()} doneAt={new Date()} />
-                    <Task desc='Tarefas pendentes'
-                        estimateAt={new Date()} doneAt={null} />
-                    <Task desc='Tarefa concluida'
-                        estimateAt={new Date()} doneAt={new Date()} />
-                    <Task desc='Tarefas pendentes'
-                        estimateAt={new Date()} doneAt={null} />
-                    <Task desc='Tarefa concluida'
-                        estimateAt={new Date()} doneAt={new Date()} />                
+                <View style={styles.taksContainer}>
+                    <FlatList data={this.state.visibleTasks}
+                        keyExtractor={item => `${item.id}`}
+                        renderItem={({ item }) => <Task {...item} toggleTask={this.toggleTask} />} >
+                    </FlatList>
                 </View>
             </View>
         )
     }
 }
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
@@ -58,15 +148,15 @@ const style = StyleSheet.create({
         justifyContent: 'flex-end',
     },
     title: {
-        fontFamily: commonStyle.fontfamily,
-        color: commonStyle.colors.secondary,
+        fontFamily: commonStyles.fontfamily,
+        color: commonStyles.colors.secondary,
         fontSize: 50,
         marginLeft: 20,
         marginBottom: 10,
     },
     subtitle: {
-        fontFamily: commonStyle.fontfamily,
-        color: commonStyle.colors.secondary,
+        fontFamily: commonStyles.fontfamily,
+        color: commonStyles.colors.secondary,
         fontSize: 20,
         marginLeft: 20,
         marginBottom: 30,
@@ -74,5 +164,11 @@ const style = StyleSheet.create({
     },
     taksContainer: {
         flex: 7,
+    },
+    iconBar: {
+        marginTop: Platform.OS === 'ios' ? 30 : 20,
+        marginHorizontal: 20,
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
     }
 })
